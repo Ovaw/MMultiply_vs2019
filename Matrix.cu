9db161dcf4edfe4798a0eca831e3e083b416c07f
ovaw <kei99dr@gmail.com>
2021-12-06 04:42:26 +0900

    add measuring calculation time with std::chrono

--- Matrix.cu
+++ Matrix.cu
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
+#include <chrono>
 
 #include <cuda_runtime.h>
-#include <timer.h>
 
 #define BLOCK 16 // 各ブロックは16 x 16個のスレッドから定義されるものとする．
 #define WIDTH 1024 // 処理対象の行列のサイズはWIDTH x WIDTH.
 
-// ホスト(CPU)側の行列定義．
+// ホスト(CPU)側の行列定義．                   
 float h_A[WIDTH * WIDTH];
 float h_B[WIDTH * WIDTH];
 float h_C[WIDTH * WIDTH];
@@ -34,26 +34,30 @@ int main()
 		h_B[i] = (float)i;
 	}
 
-	// 計算時間計測用のタイマーのセット．
-	StartTimer();
+	// 計算時間の計測開始．
+	auto d_start = std::chrono::system_clock::now();
 
 	// ホスト側の行列のデータをデバイス側の行列へ転送．
-	cudaMemcpy(d_A, h_A, sizeof(float) * WIDTH * WIDTH);
-	cudaMemcpy(d_B, h_B, sizeof(float) * WIDTH * WIDTH);
+	cudaMemcpy(d_A, h_A, sizeof(float) * WIDTH * WIDTH, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, h_B, sizeof(float) * WIDTH * WIDTH, cudaMemcpyHostToDevice);
 
 	// グリッドとブロックの定義．
 	dim3 grid(WIDTH / BLOCK, WIDTH / BLOCK);
 	dim3 block(BLOCK, BLOCK);
 
 	// GPU処理の起動．
-	d_multiply0 <<< grid, block >> > (d_A, d_B, d_C);
+	d_multiply0 <<< grid, block >>> (d_A, d_B, d_C);
 
 	// 計算結果はd_cに格納されているので，それをホスト側のh_Cに転送．
-	cudaMemcpy(h_c, d_c, sizeof(float) * WIDTH * WIDTH, cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_C, d_C, sizeof(float) * WIDTH * WIDTH, cudaMemcpyDeviceToHost);
+
+	// 計算時間の計測終了．
+	auto d_end = std::chrono::system_clock::now();
+	auto d_calcTime = d_end - d_start;
 
 	// 計算結果の表示．
-	std::cout << "デバイス計算時間: " << GetTimer() << "(ms) ";
-	std::cout << "デバイス計算結果: " << h_c[WIDTH * WIDTH - 1] << std::endl;
+	std::cout << "デバイス計算時間: " << std::chrono::duration_cast<std::chrono::milliseconds>(d_calcTime).count() << "(ms) ";
+	std::cout << "デバイス計算結果: " << h_C[WIDTH * WIDTH - 1] << std::endl;
 
 	// デバイス側のメモリを解放．
 	cudaFree(d_A);
@@ -61,9 +65,11 @@ int main()
 	cudaFree(d_C);
 
 	// 比較用にホスト側でも計算してみる．
-	StartTimer();
+	auto h_start = std::chrono::system_clock::now();
 	h_multiply(h_A, h_B, h_C);
-	std::cout << " ホスト計算時間: " << GetTimer() << " ";
+	auto h_end = std::chrono::system_clock::now();
+	auto h_calcTime = h_end - h_start;
+	std::cout << " ホスト計算時間: " << std::chrono::duration_cast<std::chrono::milliseconds>(h_calcTime).count() << "(ms) ";
 	std::cout << " ホスト計算結果: " << h_C[WIDTH * WIDTH - 1] << std::endl;
 }
 
@@ -79,4 +85,16 @@ void h_multiply(float* A, float* B, float* C)
 			C[WIDTH * r + c] = tmp;
 		}
 	}
+}
+
+__global__ void d_multiply0(float* A, float* B, float* C)
+{
+	unsigned int r = blockDim.y * blockIdx.y + threadIdx.y;
+	unsigned int c = blockDim.x * blockIdx.x + threadIdx.x;
+	unsigned int i;
+	float tmp;
+	tmp = 0.0f;
+	for (i = 0; i < WIDTH; i++)
+		tmp += A[WIDTH * r + i] * B[WIDTH * i + c];
+	C[WIDTH * r + c] = tmp;
 }
